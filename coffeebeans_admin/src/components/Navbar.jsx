943bc163751b0da0b4f3737ae50e087d3f5fbd26
Denys <denysgolenko@gmail.com>
2025-06-05 01:47:50 +0200

    bugfix

--- coffeebeans_admin/src/components/Navbar.jsx
+++ coffeebeans_admin/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import coffeeBeansIcon from '../assets/icons/coffeeBeansIcon.svg'
 
 const Navbar = ({ setToken }) => {
     return (
         <div className='flex justify-between items-center py-4 px-[4%] bg-gray-100'>
             <div className='flex items-center gap-2'>
-                <img src="/src/assets/icons/coffeeBeansIcon.svg" alt="Logo" className='w-10' />
+                <img src={coffeeBeansIcon} alt="Logo" className='w-10' />
                 <a href="/" className='text-lg font-bold sm:text-3xl font-[Fraunces]'>CoffeeBeans</a>
             </div>
             <button onClick={() => setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded text-xs sm:text-sm cursor-pointer'>logout</button>
